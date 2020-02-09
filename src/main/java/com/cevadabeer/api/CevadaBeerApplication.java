f711e2090d23138b8de2c1aa49581e8333074fcc
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-02-09 14:03:24 -0300

    java version

--- src/main/java/com/cevadabeer/api/CevadaBeerApplication.java
+++ src/main/java/com/cevadabeer/api/CevadaBeerApplication.java
@@ -2,9 +2,17 @@ package com.cevadabeer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class CevadaBeerApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CevadaBeerApplication.class, args);
